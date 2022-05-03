4afc79068eb945cfbb9338bd922d9eba9aec4fa7
djaya <djayadhinata@gmail.com>
2022-05-03 19:21:51 +0700

    init commit

---
+++ src/components/Footer.vue
@@ -0,0 +1,74 @@
+<template>
+  <footer>
+    <div class="container-fluid" style="background: white;">
+      <div class="row p-4">
+        <div class="col-md-4">
+          <h5>TENTANG</h5>
+          <hr />
+          <p>
+            SMKS Globe 2.
+          </p>
+        </div>
+        <div class="col-md-4">
+          <h5>TAGS</h5>
+          <hr />
+          <div v-if="tags.length > 0">
+            <router-link
+              :to="{ name: 'detail_tag', params: { slug: tag.slug } }"
+              v-for="tag in tags"
+              :key="tag.id"
+              class="btn btn-sm btn-outline-secondary mb-2 me-2"
+              >{{ tag.name.toUpperCase() }}</router-link
+            >
+          </div>
+          <div v-else class="text-center">
+            <ListLoader />
+          </div>
+        </div>
+        <div class="col-md-4">
+          <h5>KONTAK</h5>
+          <hr />
+          <p>
+            <i class="fa fa-map-marker" aria-hidden="true"></i>ase Camp, Komplek
+            Batu Aji Center Park Simpang, Tanjung Uncang, Batu Aji, Batam City,
+            Riau Islands 29425 <br />
+            <i class="fas fa-phone"></i> 0778-391333
+          </p>
+        </div>
+      </div>
+    </div>
+    <div class="container-fluid bg-dark">
+      <div class="row p-3">
+        <div class="text-center text-white font-weight-bold">
+          Copyright © 2022 SMKS Globe 2. All rights reserved.
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+//import content loader
+import { ListLoader } from "vue-content-loader";
+
+//import axios
+import axios from "axios";
+
+export default {
+  name: "Footer",
+  components: {
+    //loader component
+    ListLoader,
+  },
+  data() {
+    return {
+      tags: [],
+    };
+  },
+  created() {
+    axios.get("/api/tag").then((response) => {
+      this.tags = response.data.data.data;
+    });
+  },
+};
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,58 @@
+<template>
+  <nav
+    class="navbar navbar-expand-md navbar-dark fixed-top bg-dark"
+    style="border-top: 5px solid #0d106e;"
+  >
+    <div class="container-fluid">
+      <router-link :to="{ name: 'home' }" class="navbar-brand">
+        SMKS Globe 2</router-link
+      >
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarCollapse"
+        aria-controls="navbarCollapse"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarCollapse">
+        <ul class="navbar-nav mr-auto mb-2 mb-md-0">
+          <li class="nav-item">
+            <router-link :to="{ name: 'post' }" class="nav-link">
+              BERITA</router-link
+            >
+          </li>
+          <li class="nav-item">
+            <router-link :to="{ name: 'event' }" class="nav-link">
+              AGENDA</router-link
+            >
+          </li>
+          <li class="nav-item">
+            <router-link :to="{ name: 'photo' }" class="nav-link">
+              GALERI</router-link
+            >
+          </li>
+          <li class="nav-item">
+            <router-link :to="{ name: 'video' }" class="nav-link">
+              VIDEO</router-link
+            >
+          </li>
+          <li class="nav-item">
+            <router-link :to="{ name: 'contact' }" class="nav-link">
+              KONTAK</router-link
+            >
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
